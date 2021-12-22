d69a375a016037e3899c3d6b5d0a29d29325eb92
Pedro Henrique <pedro.s.henrique2002@gmail.com>
2021-12-22 14:47:08 -0300

    Inclusão e melhorias
    - melhorias banner

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
-// $('.slider-principal').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 300,
-//     slidesToShow: 4
-// });
-
 $('.slider-principal').slick({
     dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
-    adaptiveHeight: true
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
   });
